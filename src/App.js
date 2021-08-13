78d7803c6eed02a7128f0165c2a2e6c30aa794e1
surajverma2587 <surajverma2587@gmail.com>
2021-08-13 19:47:26 +0100

    setup material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Button from "@material-ui/core/Button";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
 };
 
 export default App;
